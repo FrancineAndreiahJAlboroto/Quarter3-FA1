0dc76a774bbf7c42b2c1aa4909523428f6429df6
FrancineAndreiahJAlboroto <francineandreiah.alboroto@smc.pshs.edu.ph>
2025-01-29 20:34:21 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,7 +2,7 @@ var n = prompt("Enter your nickname:");
 var h = parseFloat(prompt("Enter your height (in inches)", "0"));
 var w = parseFloat(prompt("Enter your weight (in kilograms)", "0"));
    
-var wfinal = (w*2.205).toFixed(3);
+var wfinal = (w*2.20462).toFixed(3);
 var r = h % 12;
 var hfinal = (h/12).toFixed(0);
    
